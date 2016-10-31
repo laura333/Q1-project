1341888a817d4cb9d3fe8cf1e992daf59a890fdd
Laura Baukol <laura.baukol@mac.com>
2016-10-31 16:15:17 -0600

    created second page

--- js/init.js
+++ js/init.js
@@ -1,8 +1,31 @@
-(function($){
-  $(function(){
+'use strict';
 
+// (function($) {
+//     $(function() {
+//
+//     }); // end of document ready
+// })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+
+    console.log('Let\'s do this!');
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal').modal();
+    $('.gender').on('click', function() {
+        console.log('????????')
+    });
+    // var genderBtns = document.getElementsByClassName('gender')
+    // for (var i = 0; i < genderBtns.length; i++) {
+    //   genderBtns[i].addEventListener('click',function () {
+    //     console.log('it was clicked');
+    //   })
+    // }
+    // console.log(genderBtns);
+    // $('h2').click(function () {
+    //   alert('it was clicked!!!!')
+    // })
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+
+
+})
