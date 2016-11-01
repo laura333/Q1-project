6f4e75dcce0839438c3675dccfd85ac839106f1e
Laura Baukol <laura.baukol@mac.com>
2016-11-01 10:58:45 -0600

    deployed to surge @ comicfy-me.surge.sh

--- js/init.js
+++ js/init.js
@@ -8,51 +8,54 @@
 
 $(document).ready(function() {
 
+// testing data.js data
+//    console.log('error', data.error);
+
     console.log('Let\'s do this!');
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.modal').modal();
     $('.gender').on('click', function() {
-        console.log('clicked!')
+        console.log('gender!')
+        
+    });
+    $('.origin').on('click', function() {
+        console.log('origin!')
     });
 
-    // $('.btn-large-test').on('click', function(event){
-    //   event.preventDefault();
-    //   // var search = $('#search').val();
-    //   // console.log(search);
-    //
-    //   // var $xhr = $.getJSON('http://comicvine.gamespot.com/api/characters?api_key=19b875b3fb7d4abe44bbf49506248a975491895f&format=json');
-    //   var $xhr = $('JSON.json');
-    //   console.log($xhr);
-    //
-    //   $xhr.done(function(data) {
-    //       if ($xhr.status !== 200) {
-    //           return;
-    //       }
-    // for (var i = 0; i < data.Search.length; i++) {
-    //   var stuff = data.Search[i];
-    //
-    //   console.log(stuff.Poster, stuff.Title, stuff.imdbID, stuff.Year);
-    //
-    // }
-    //   });
+    $('.btn-large-test').on('click', function(event){
+      event.preventDefault();
 
-      // $xhr.fail(function(err) {
-      //     console.log(err);
-      // });
+      var $xhr = $.getJSON('https://g-comicvine.herokuapp.com/api/characters?api_key=19b875b3fb7d4abe44bbf49506248a975491895f&format=JSON');
+      // var $xhr = $(data);
+      console.log($xhr);
 
-    // });
-    // var genderBtns = document.getElementsByClassName('gender')
-    // for (var i = 0; i < genderBtns.length; i++) {
-    //   genderBtns[i].addEventListener('click',function () {
+      $xhr.done(function(data) {
+          if ($xhr.status !== 200) {
+              return;
+          }
+          console.log(data);
+
+    for (var i = 0; i < data.results.length; i++) {
+      var stuff = data.results[i];
+      if(stuff.gender && stuff.origin.name) {
+          console.log(stuff.gender, stuff.origin.name);
+      };
+
+    }
+      });
+
+      $xhr.fail(function(err) {
+          console.log(err);
+      });
+
+    });
+
+    // var gender = document.getElementsByClassName('gender')
+    // for (var i = 0; i < gender.length; i++) {
+    //   gender[i].addEventListener('click',function () {
     //     console.log('it was clicked');
     //   })
     // }
-    // console.log(genderBtns);
-    // $('h2').click(function () {
-    //   alert('it was clicked!!!!')
-    // })
 
-
-
-})
+});
